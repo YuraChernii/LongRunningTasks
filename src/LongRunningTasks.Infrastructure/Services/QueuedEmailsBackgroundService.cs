928da07f9ff5a52b3568c45e9a1e2981e73aed5f
Yura Chernii <mik373737373737@gmail.com>
2023-04-10 08:19:23 -0700

    fix

--- src/LongRunningTasks.Infrastructure/Services/QueuedEmailsBackgroundService.cs
+++ src/LongRunningTasks.Infrastructure/Services/QueuedEmailsBackgroundService.cs
@@ -185,7 +185,7 @@ namespace LongRunningTasks.Infrastructure.Services
                     }
                     else if (message.From.Mailboxes.Any(_ => _.Address.Contains("e-noreply@land.gov.ua")) &&
                                                         text != null &&
-                                                        text.ToLower().Contains("Про внесення відомостей (змін до них) до Державного земельного кадастру за кадастровим номером")
+                                                        text.ToLower().Contains("про внесення відомостей (змін до них) до державного земельного кадастру за кадастровим номером")
                                                   )
                     {
                         int index = text.IndexOf("\\r\\n") - 1;
